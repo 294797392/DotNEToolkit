66fe5acf6d2438845886f432e5e9a7b2fb21479a
oheiheiheiheihei <294797392@qq.com>
2020-08-21 17:33:36 +0800

    .

---
+++ Sources/DotNEToolkit/JSONHelper.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNEToolkit
+{
+    /// <summary>
+    /// JSON函数
+    /// </summary>
+    public static class JSONHelper
+    {
+    }
+}
---
+++ Sources/DotNEToolkit/ResponseCode.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNEToolkit
+{
+    /// <summary>
+    /// DotNEToolkit的返回值
+    /// </summary>
+    public static class ResponseCode
+    {
+        /// <summary>
+        /// 成功
+        /// </summary>
+        public const int Success = 1;
+    }
+}
